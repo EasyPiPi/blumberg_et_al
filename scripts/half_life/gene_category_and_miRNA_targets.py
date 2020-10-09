9a4f352bc7a317791d03bf293f3eeea2b7a7119d
Yixin Zhao <gzbyzyx@126.com>
2020-10-08 20:09:30 -0400

    bootstrap analysis for RNA half-life

--- scripts/half_life/gene_category_and_miRNA_targets.py
+++ scripts/half_life/gene_category_and_miRNA_targets.py
@@ -19,6 +19,8 @@ import pylab
 
 import statsmodels.formula.api as smf
 from scipy import stats
+from statsmodels.distributions.empirical_distribution import ECDF
+from sklearn.utils import resample
 
 from math import isnan
 
@@ -100,13 +102,14 @@ def target_plot_df(miRNA_name):
 # CDF
 
 def CDF_plot(df, xlim=(-7,5), color = [cm.tab20(6), cm.tab20(7), cm.tab20(0), cm.tab20(1)]):
-    g = sns.FacetGrid(df, hue="variable", height = 5, palette= color, legend_out=False, xlim=xlim, aspect = 1.2)
-    # g = sns.FacetGrid(df, hue="variable", height = 5, palette= color, legend_out=False)
-    g = g.map(sns.distplot, "value", kde_kws=dict(cumulative=True), hist = False)
+    g = sns.FacetGrid(df, hue="variable", height = 5, palette=color, legend_out=False, xlim=xlim, aspect = 1.2)
+    # g = g.map(sns.distplot, "value", kde_kws=dict(cumulative=True), hist = False)
+    g = g.map(sns.ecdfplot, "value")
+
     g.add_legend(title = '')
     plt.legend(loc=4)
     plt.xlabel(r'log$_2$$($T$_1$$_/$$_2$$^P$$^R$$)$')
-    plt.ylabel('CDF')
+    plt.ylabel('ECDF')
     plt.tight_layout()
 
 #KS_test_target_number_summary = DataFrame(columns = ['miRNA_name', 'group_1_p', 'group_2_p', 'target_number', 'non_target_number'])
@@ -193,11 +196,111 @@ def gencat_plot_df(gene_name_1, gene_name_2, name_1, name_2, name_3):
 
     return df_targets_plot_long
 
-df_targets_plot_long= gencat_plot_df(ribosomal_proteins['Ensembl gene ID'], zinc_fingers['Ensembl gene ID'], 'Ribosomal proteins', 'Zinc-fingers', 'Others')
+df_targets_plot_long = gencat_plot_df(ribosomal_proteins['Ensembl gene ID'], zinc_fingers['Ensembl gene ID'], 'Ribosomal proteins', 'Zinc-fingers', 'Others')
 
 #style.available
-#sns.set_style(style.use("ggplot"))
-sns.set(font_scale=1.3)
+#sns.set(font_scale=1.3)
+#sns.set_style("ticks")
+#CDF_plot(df_targets_plot_long, xlim=(-7, 7), color=None)
+#pylab.savefig(os.path.join(genecat_fig_dir, "ribosomal_proteins_and_zinc_fingers_proteins_half_life_CDF.pdf"))
+
+#### Use Bootstrap to plot 95% confidence interval ####
+#### for ribosomal proteins and zinc-fingers ####
+# Step 1 Bootstrap
+# https://stackoverflow.com/questions/49328319/random-sampling-from-a-column-several-times-in-python-pandas
+# https://stackoverflow.com/questions/36390406/sample-each-group-after-pandas-groupby
+# a generator function
+def sample_rep(dframe, replicates=None):
+    for i in range(replicates):
+        # resample half-life within each gene group
+        yield dframe.groupby("variable").apply(lambda x: x.sample(frac=1, replace=True)).reset_index(drop=True)
+
+d = pd.concat(sample_rep(df_targets_plot_long, replicates=1000), keys=range(1, 1001), names=["replicate"])
+
+# Step 2 Calculate ECDF
+#def ecdf(data):
+#    """ Compute ECDF """
+#    x = np.sort(data)
+#    n = x.size
+#    y = np.arange(1, n+1) / n
+#    return(x,y)
+# https://www.statsmodels.org/stable/generated/statsmodels.distributions.empirical_distribution.ECDF.html?highlight=ecdf#statsmodels.distributions.empirical_distribution.ECDF
+
+def compute_ecdf(df):
+    df["ecdf"] = ECDF(df.value)(df.value)
+    return df
+
+d = d.groupby(["replicate","variable"]).apply(compute_ecdf)
+d = d.reset_index(level="replicate").reset_index(drop=True)
+
+# Step 3 compute Confidential Interval for ECD
+def compute_ci(df):
+    x, y = np.percentile(df.ecdf, q = [2.5, 97.5])
+    z = np.mean(df.ecdf)
+    return pd.Series(data = [x, y, z], index = ["lower_bound", "upper_bound", "mean"])
+
+d = d.groupby(["variable", "value"]).apply(compute_ci)
+d = d.reset_index()
+
+# Step 4 Customized function to plot the CIs
+def plot_ci(category, alpha=0.2):
+    plt.fill_between(d.loc[d.variable == category, "value"], d.loc[d.variable == category, "upper_bound"], d.loc[d.variable == category, "lower_bound"], alpha=alpha)
+
+sns.set(font_scale=1.2)
 sns.set_style("ticks")
-CDF_plot(df_targets_plot_long, xlim=(-7, 7), color=None)
+
+g = sns.relplot(data=d, x="value", y="mean", hue="variable", kind="line", ci=None, height=5, aspect=0.8)
+g.set(xlim=(-7, 7))
+g._legend.set_title("")
+g._legend.set_bbox_to_anchor([1, 0.3])
+plot_ci("Others")
+plot_ci("Ribosomal proteins")
+plot_ci("Zinc-fingers")
+plt.xlabel(r'log$_2$$($T$_1$$_/$$_2$$^P$$^R$$)$')
+plt.ylabel('ECDF')
+plt.tight_layout()
 pylab.savefig(os.path.join(genecat_fig_dir, "ribosomal_proteins_and_zinc_fingers_proteins_half_life_CDF.pdf"))
+plt.close()
+
+#### for several selected miRNAs ####
+KS_test_target_number_summary = \
+    DataFrame(columns = ['miRNA_name', 'group_1_p', \
+                         'target_number', 'non_target_number'])
+
+selected_miRs = ["miR-182-5p", "miR-125-5p", "miR-19-3p"]
+
+i = 0
+
+for miRNA_name in selected_miRs:    
+    print(miRNA_name)
+    df_targets_plot_long, group_1_p, target_number, non_target_number = target_plot_df(miRNA_name) 
+    print("--------------------------------")
+    KS_test_target_number_summary.loc[i, 'miRNA_name'] = miRNA_name
+    KS_test_target_number_summary.loc[i, 'group_1_p'] = group_1_p
+    KS_test_target_number_summary.loc[i, 'target_number'] = target_number
+    KS_test_target_number_summary.loc[i, 'non_target_number'] = non_target_number
+    
+    # Bootstrap and visualization
+    # Step 1 Bootstrap
+    d = pd.concat(sample_rep(df_targets_plot_long, replicates=1000), keys=range(1, 1001), names=["replicate"])
+    # Step 2 Calculate ECDF
+    d = d.groupby(["replicate","variable"]).apply(compute_ecdf)
+    d = d.reset_index(level="replicate").reset_index(drop=True)
+    # Step 3 compute Confidential Interval for ECD
+    d = d.groupby(["variable", "value"]).apply(compute_ci)
+    d = d.reset_index()
+    # Step 4 visualization
+    g = sns.relplot(data=d, x="value", y="mean", hue="variable", kind="line", ci=None, height=5, aspect=0.7)
+    g.set(xlim=(-7, 7))
+    g._legend.set_title("")
+    g._legend.set_bbox_to_anchor([1, 0.3])
+    plot_ci(d.variable.unique()[0])
+    plot_ci(d.variable.unique()[1])
+    plt.xlabel(r'log$_2$$($T$_1$$_/$$_2$$^P$$^R$$)$')
+    plt.ylabel('ECDF')
+    plt.tight_layout()
+    pylab.savefig(os.path.join(mir_fig_dir, ("_").join(miRNA_name.split("/")) + "_targets_half_life_ECDF.pdf"))
+    plt.close()
+    
+    i = i+1
+
